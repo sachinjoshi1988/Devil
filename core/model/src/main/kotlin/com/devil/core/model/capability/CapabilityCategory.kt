6a2848e8ae1edff538beec4ca150cd48bd4ad376
Sachin Joshi <peshwa16588@gmail.com>
2026-08-01 11:14:13 +0530

    Stage 1: Add capability category

---
+++ core/model/src/main/kotlin/com/devil/core/model/capability/CapabilityCategory.kt
@@ -0,0 +1,14 @@
+package com.devil.core.model.capability
+
+/**
+ * Classifies the constitutional purpose of a registered Devil capability.
+ *
+ * This category does not imply availability, authorization, health, or
+ * permission to execute.
+ */
+enum class CapabilityCategory {
+    INPUT,
+    OUTPUT,
+    ACTION,
+    KNOWLEDGE,
+}
